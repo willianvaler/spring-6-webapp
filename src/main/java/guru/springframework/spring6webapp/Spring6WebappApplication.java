bd43d226c79fefd6bba1aa75d4ca46b94d8dd66b
willianvaler <willianvaler@gmail.com>
2024-06-25 20:06:40 -0300

    trying out spring context

--- src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
+++ src/main/java/guru/springframework/spring6webapp/Spring6WebappApplication.java
@@ -1,13 +1,21 @@
 package guru.springframework.spring6webapp;
 
+import guru.springframework.spring6webapp.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring6WebappApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Spring6WebappApplication.class, args);
+    public static void main(String[] args)
+    {
+        ApplicationContext run = SpringApplication.run( Spring6WebappApplication.class, args );
+
+        MyController controller = run.getBean( MyController.class );
+
+        System.out.println( "main method " );
+        System.out.println( controller.sayHello() );
     }
 
 }
