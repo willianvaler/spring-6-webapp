bbf10f8caebcb8de3436a9371b077dc55918a088
willianvaler <willianvaler@gmail.com>
2024-06-25 20:14:42 -0300

    testing autowired context on tests

--- src/test/java/guru/springframework/spring6webapp/Spring6WebappApplicationTests.java
+++ src/test/java/guru/springframework/spring6webapp/Spring6WebappApplicationTests.java
@@ -1,13 +1,38 @@
 package guru.springframework.spring6webapp;
 
+import guru.springframework.spring6webapp.controllers.MyController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
-class Spring6WebappApplicationTests {
+class Spring6WebappApplicationTests
+{
+    @Autowired
+    ApplicationContext context;
+
+    @Autowired
+    MyController controller;
 
     @Test
-    void contextLoads() {
+    void testAutoWiredOfController()
+    {
+           System.out.println(controller.sayHello());
+    }
+
+    @Test
+    void testGetControllerFromCtx()
+    {
+        MyController controller = context.getBean(MyController.class);
+
+        System.out.println( controller.sayHello() );
+    }
+
+    @Test
+    void contextLoads()
+    {
+
     }
 
 }
