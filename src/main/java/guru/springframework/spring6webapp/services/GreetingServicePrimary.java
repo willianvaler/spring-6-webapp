55148c6b8295cd53d556b05c1d7352f05e3f75c5
willianvaler <willianvaler@gmail.com>
2024-06-25 22:13:56 -0300

    primary bean

--- src/main/java/guru/springframework/spring6webapp/services/GreetingServicePrimary.java
+++ src/main/java/guru/springframework/spring6webapp/services/GreetingServicePrimary.java
@@ -1,2 +1,15 @@
-package guru.springframework.spring6webapp.services;public class GreetingServicePrimary {
+package guru.springframework.spring6webapp.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Primary //como temos 2 implementações de GreetingService, o teste falha,mas o @Primary faz com que esta classe seja o primeiro bean que seja injetado
+@Service
+public class GreetingServicePrimary implements GreetingService
+{
+    @Override
+    public String sayGreeting()
+    {
+        return " hello from primaey bean";
+    }
 }
